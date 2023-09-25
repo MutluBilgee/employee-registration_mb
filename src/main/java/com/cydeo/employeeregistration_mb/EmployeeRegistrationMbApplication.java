c315b9586a892163cb29c54810305d952b4c99fa
MutluBilgee <mutlubilgee@gmail.com>
2023-09-25 07:57:28 +0200

    Initial commit

---
+++ src/main/java/com/cydeo/employeeregistration_mb/EmployeeRegistrationMbApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregistration_mb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistrationMbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRegistrationMbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/employeeregistration_mb/EmployeeRegistrationMbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregistration_mb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationMbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
