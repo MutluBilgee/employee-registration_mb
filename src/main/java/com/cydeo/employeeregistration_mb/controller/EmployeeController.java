fc005890d57a4d50a398f5a651347c2c1a470e88
MutluBilgee <mutlubilgee@gmail.com>
2023-09-26 22:55:28 +0200

    Employee registration form has been updated

--- src/main/java/com/cydeo/employeeregistration_mb/controller/EmployeeController.java
+++ src/main/java/com/cydeo/employeeregistration_mb/controller/EmployeeController.java
@@ -1,9 +1,16 @@
 package com.cydeo.employeeregistration_mb.controller;
 
+import com.cydeo.employeeregistration_mb.bootstrap.DataGenerator;
+import com.cydeo.employeeregistration_mb.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -12,11 +19,28 @@ public class EmployeeController {
     @GetMapping("/register")
     public String createEmployee(Model model) {
 
-//        model.addAttribute("employee", new Employee());
-//        model.addAttribute("states", DataGenerator.getAllStates());
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("states", DataGenerator.getAllStates());
 
         return "employee/employee-create";
 
+    }
+
+    @PostMapping("/list")
+    public String employeeList(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("states", DataGenerator.getAllStates());
+
+            return "employee/employee-create";
+        }
+
+        DataGenerator.saveEmployee(employee);
+
+        model.addAttribute("employees", DataGenerator.readAllEmployees());
+
+        return "employee/employee-list";
     }
 
 }
